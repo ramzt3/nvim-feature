f2ce5f328183adcd89e0e8f3ae8ed73486e6e027
Siliphong Lathsavong <jukhaha3@gmail.com>
2026-03-25 14:52:43 +0700

    update

--- lua/pack/init.lua
+++ lua/pack/init.lua
@@ -3,18 +3,18 @@
 vim.pack.add({
     -- {src = 'https://github.com/rose-pine/neovim'},
     -- {src = 'https://github.com/scottmckendry/cyberdream.nvim'},
-    {src = 'https://github.com/folke/which-key.nvim'},
-    {src = 'https://github.com/VonHeikemen/ts-enable.nvim'},
-    {src = 'https://github.com/neovim/nvim-lspconfig'},
-    {src = 'https://github.com/nvim-mini/mini.nvim', version = 'main'},
+    { src = 'https://github.com/folke/which-key.nvim' },
+    { src = 'https://github.com/VonHeikemen/ts-enable.nvim' },
+    { src = 'https://github.com/neovim/nvim-lspconfig' },
+    { src = 'https://github.com/nvim-mini/mini.nvim', version = 'main' },
     {
         src = 'https://github.com/nvim-treesitter/nvim-treesitter',
         version = 'main',
         data = {
-          on_update = function()
-            vim.cmd('TSUpdate')
-          end,
-    },
+            on_update = function()
+                vim.cmd('TSUpdate')
+            end,
+        },
     },
     { src = "https://github.com/windwp/nvim-ts-autotag" },
     { src = "https://github.com/windwp/nvim-autopairs" },
@@ -28,14 +28,14 @@ vim.pack.add({
     { src = "https://github.com/folke/trouble.nvim" },
     { src = "https://github.com/j-hui/fidget.nvim" },
     { src = "https://github.com/gelguy/wilder.nvim" },
-    "https://github.com/nvim-tree/nvim-web-devicons",
-	"https://github.com/romgrk/fzy-lua-native",
-	"https://github.com/mbbill/undotree",
-	"https://github.com/kdheepak/lazygit.nvim",
-	"https://github.com/b0o/incline.nvim",
-	"https://github.com/folke/todo-comments.nvim",
-    "https://github.com/lukas-reineke/indent-blankline.nvim",
-    "https://github.com/christoomey/vim-tmux-navigator",
-    "https://github.com/lewis6991/gitsigns.nvim",
+    { src = "https://github.com/nvim-tree/nvim-web-devicons" },
+    { src = "https://github.com/romgrk/fzy-lua-native" },
+    { src = "https://github.com/mbbill/undotree" },
+    { src = "https://github.com/kdheepak/lazygit.nvim" },
+    { src = "https://github.com/b0o/incline.nvim" },
+    { src = "https://github.com/folke/todo-comments.nvim" },
+    { src = "https://github.com/lukas-reineke/indent-blankline.nvim" },
+    { src = "https://github.com/christoomey/vim-tmux-navigator" },
+    { src = "https://github.com/lewis6991/gitsigns.nvim" },
 })
 
