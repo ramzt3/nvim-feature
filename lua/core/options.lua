49f20c86fa068fb3ab2f87fcb474d977ccd29b92
Siliphong Lathsavong <jukhaha3@gmail.com>
2026-04-20 14:05:50 +0700

    highlight

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -12,7 +12,7 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.autoindent = true
-vim.opt.smartindent = true
+vim.opt.smartindent = false
 vim.opt.wrap = true
 
 vim.opt.swapfile = false
